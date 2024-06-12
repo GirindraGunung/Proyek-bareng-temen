79c0e471ad6523bcb762b78a3ad4f68dfa57ed43
Koko <girindra220420@gmail.com>
2024-06-12 18:27:36 +0700

    Player animation and level

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,9 +2,28 @@ extends CharacterBody2D
 
 #VARIABLES
 var speed = 300
+@onready var animation = $animation
+@onready var hitbox = $hitbox
+
+
 
 #MOVEMENT
 func _physics_process(delta):
 	var input = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
 	velocity = speed*input
+	#ANIMATION
+	if velocity.x or velocity.y != 0:
+		if velocity.x < 0:
+			animation.flip_h = true
+			animation.play("walk")
+			#HITBOX_SET
+			hitbox.position.x = -1.5
+		else:
+			animation.flip_h = false
+			animation.play("walk")
+			#HITBOX_SET
+			hitbox.position.x = 1.5
+	else:
+		animation.play("idle")
+	print(hitbox.position.x)
 	move_and_slide()
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
+#VARIABLES
 var player_Res = preload("res://Scenes/player.tscn")
 @onready var spawn_pos = $spawn_Pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#PLAYER SET
 	var player = player_Res.instantiate()
 	add_child(player)
 	player.global_position = spawn_pos.global_position
 
+func _process(delta):
+	#EXIT(sementara)
+	if Input.is_action_just_pressed("pause"):
+		get_tree().quit()
