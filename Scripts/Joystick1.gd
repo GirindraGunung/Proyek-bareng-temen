030954e094bc01f8d7618799cd7da2cd7c0ab97c
Koko <girindra220420@gmail.com>
2024-06-19 17:48:59 +0700

    joystick sens

--- Scripts/Joystick1.gd
+++ Scripts/Joystick1.gd
@@ -13,33 +13,36 @@ func _ready():
 func _process(delta):
 	#JOYSTICK
 	if pressing:
+		var angle = joystick.global_position.angle_to_point(get_global_mouse_position())
 		if get_global_mouse_position().distance_to(joystick.global_position) <= diameter:
 			global_position = get_global_mouse_position()
-			direction()
+			direction(angle)
 		else:
-			var angle = joystick.global_position.angle_to_point(get_global_mouse_position())
 			global_position.x = joystick.global_position.x + (cos(angle)*diameter)
 			global_position.y = joystick.global_position.y + (sin(angle)*diameter)
-			direction()
+			direction(angle)
 	else:
 		global_position = joystick.global_position
 		joystick.direction = Vector2.ZERO
 
-func direction():
+func direction(input_Angle):
+	var angle = input_Angle
 	#X_DIRECTION
-	if position.y < 75 and position.y > -75:
+	if angle < 0.95 and angle > -0.95:
 		if (global_position.x - joystick.global_position.x) >= deadZone:
 			joystick.direction.x = 1
-		elif (global_position.x - joystick.global_position.x) <= -deadZone:
+	elif angle > 2.2 or angle < -2.2:
+		if (global_position.x - joystick.global_position.x) <= -deadZone:
 			joystick.direction.x = -1
 	else :
 		joystick.direction.x = 0
 	
 	#Y_DIRECTION
-	if position.x < 75 and position.x > -75:
+	if angle > 0.6 and angle < 2.5:
 		if (global_position.y - joystick.global_position.y) >= deadZone:
 			joystick.direction.y = 1
-		elif (global_position.y - joystick.global_position.y) <= -deadZone:
+	elif angle < -0.6 and angle > -2.5:
+		if (global_position.y - joystick.global_position.y) <= -deadZone:
 			joystick.direction.y = -1
 	else :
 		joystick.direction.y = 0
